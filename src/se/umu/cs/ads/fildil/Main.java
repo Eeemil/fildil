374a920805604fa250952e7969cf0a7619b0d26f
Timmy Olsson <c12ton@cs.umu.se>
2016-12-14 13:11:02 +0100

    Reading chunks

--- src/se/umu/cs/ads/fildil/Main.java
+++ src/se/umu/cs/ads/fildil/Main.java
@@ -1,87 +1,31 @@
 package se.umu.cs.ads.fildil;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         //argv0: filename for video file
         if (args.length < 1) {
             System.err.println("Supply file name!");
             System.exit(1);
         }
 
-        String video = args[0];
-        List<String> command = new ArrayList<String>();
-        command.add("ffmpeg");
-        command.add("-i");
-        command.add(video);
-        command.add("-vcodec");
-        command.add("mpeg2video");
-        command.add("-acodec");
-        command.add("mp2");
-        command.add("-b:v");
-        command.add("3M");
-        command.add("-b:a");
-        command.add("192k");
-        command.add("-muxrate");
-        command.add("10M");
-        command.add("-f");
-        command.add("asf");  //- video format (TEMP!)
-        command.add("-");
-
-//        Process processFFmpeg = null;
-//
-//        try {
-//            processFFmpeg = new ProcessBuilder(command).start();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.exit(2);
-//        }
-
-        //TEST
-        ArrayList<ByteArrayInputStream> splits = null;
+        ArrayList<byte[]> chunks = null;
         try {
-            splits = VideoProperties.split(video,5);
+            chunks = VideoProperties.toChunks(args[0],1024);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //------------
 
-
-        int n;
-        InputStream input = processFFmpeg.getInputStream();
-        byte[] buf = new byte[1024];
-        try {
-            while((n=input.read(buf)) > -1) {
-                System.out.write(buf,0,n);
+        for(byte[] c:chunks) {
+            try {
+                System.out.write(c);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        //------------
-
-
-//        int n;
-//        InputStream input = processFFmpeg.getInputStream();
-//        OutputStream out = processVLC.getOutputStream();
-//        byte[] buf = new byte[1024];
-//        try {
-//            while((n=input.read(buf)) != -1) {
-//                out.write(buf,0,n);
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-        System.out.println(VideoProperties.getTime(video));
 
         System.out.println("Done");
     }
